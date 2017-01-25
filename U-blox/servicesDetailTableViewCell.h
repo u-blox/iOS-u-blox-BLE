9d8a1d2efd27f05a384c4af1b40d6a2921363fe1
cmag <stenmo@gmail.com>
2017-01-25 15:10:17 +0100

    Updated header to u-blox version.

--- U-blox/servicesDetailTableViewCell.h
+++ U-blox/servicesDetailTableViewCell.h
@@ -1,10 +1,22 @@
-//
-//  servicesDetailTableViewCell.h
-//  U-blox
-//
-//  Created by Bill Martensson on 2015-04-10.
-//  Copyright (c) 2015 U-blox. All rights reserved.
-//
+/*
+ * Copyright (C) u-blox 
+ * 
+ * u-blox reserves all rights in this deliverable (documentation, software, etc.,
+ * hereafter “Deliverable”). 
+ * 
+ * u-blox grants you the right to use, copy, modify and distribute the
+ * Deliverable provided hereunder for any purpose without fee.
+ * 
+ * THIS DELIVERABLE IS BEING PROVIDED "AS IS", WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTY. IN PARTICULAR, NEITHER THE AUTHOR NOR U-BLOX MAKES ANY
+ * REPRESENTATION OR WARRANTY OF ANY KIND CONCERNING THE MERCHANTABILITY OF THIS
+ * DELIVERABLE OR ITS FITNESS FOR ANY PARTICULAR PURPOSE.
+ * 
+ * In case you provide us a feedback or make a contribution in the form of a
+ * further development of the Deliverable (“Contribution”), u-blox will have the
+ * same rights as granted to you, namely to use, copy, modify and distribute the
+ * Contribution provided to us for any purpose without fee.
+ */
 
 #import <UIKit/UIKit.h>
 
